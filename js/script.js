fc6f7df269a5c86a78ea9828f87e5c64f03f8415
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-26 15:46:20 +0100

    aggiunta funzione per aggingere elementi tramite campo input

--- js/script.js
+++ js/script.js
@@ -3,11 +3,18 @@ var app = new Vue(
     el: '#todo-list',
     data:{
       listaSpesa: ['Comprare il pane', 'Comprare la pasta', 'Comprare la frutta', 'Comprare la verdura'],
+      newElement: '',
     },
     methods: {
-      deleteElement: function(elementIndex) {
-        this.listaSpesa.splice(elementIndex, 1);
-      }
+      deleteElement: function(itemIndex) {
+        this.listaSpesa.splice(itemIndex, 1);
+      },
+      addElement: function() {
+        if(this.newElement !== ''){
+          this.listaSpesa.push(this.newElement);
+          this.newElement = '';
+        }
+      },
     }
 
 });
