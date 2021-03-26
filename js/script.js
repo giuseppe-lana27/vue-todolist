40391776be11edf6ea61cbae21368df4ec62fbfe
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-26 15:25:15 +0100

    aggiunta lista della spesa e funzione per eliminare gli elementi lista

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+var app = new Vue(
+  {
+    el: '#todo-list',
+    data:{
+      listaSpesa: ['Comprare il pane', 'Comprare la pasta', 'Comprare la frutta', 'Comprare la verdura'],
+    },
+    methods: {
+      deleteElement: function(elementIndex) {
+        this.listaSpesa.splice(elementIndex, 1);
+      }
+    }
 
+});
